19daa214a901584858500a91cabdfa3b420e260e
GitHub Action <action@github.com>
2022-10-08 13:50:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/zakarea/codesmells/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/zakarea/codesmells/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.zakarea.codesmells.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zakarea.codesmells.MyBundle
 
 class MyApplicationService {
 
